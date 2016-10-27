3145fbe21d949e0f8bf57fb30ac852817a6a88f0
chenfanyue <nicegogogo@163.com>
2016-10-27 11:35:25 +0800

    Create ASimpleSeperatedView.php

---
+++ ASimpleSeperatedView.php
@@ -0,0 +1,25 @@
+// Data and View combined.
+<ul>
+  <?php foreach($tasks as $task) : ?>
+  	<?php if($task->completed) : ?>
+  		<strike><?= $task->description; ?></strike>
+  	<?php else: ?>
+  		<?= $task->description; ?>
+  	<?php endif; ?>
+  <?php endforeach; ?>
+</ul>
+
+// Data and View seperated.
+<ul>
+  <?php foreach($tasks as $task) : ?>
+  	<?php if($task->completed) : ?>
+  		<strike>
+  	<?php endif; ?>
+  	
+  	<?= $task->description; ?>
+  		
+  	<?php if($task->completed) : ?>
+  		</strike>
+  	<?php endif; ?>
+  <?php endforeach; ?>
+</ul>
